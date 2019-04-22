7f85854af38e2156000a5d9a6f5d587119164910
dreamerwu <haowbdreamer@gmail.com>
2019-04-22 11:37:36 -0400

    Update HCT116_RNAseq analysis.R

--- HCT116_RNAseq analysis.R
+++ HCT116_RNAseq analysis.R
@@ -18,7 +18,22 @@ data2=data[,2:ncol(data)]
 row.names(data2)=data[,1:1]
 pheatmap(data2,cluster_cols=TRUE,cellwidth=25,colorRampPalette(c("green","black","red"))(50))
 
+#plot pathway annotation
+library("ggplot2")
+data=read.delim("D:/demo/demo.txt",head=T,sep="\t")
+ls(data)
+data$Gene.Set.Name=factor(data$Gene.Set.Name,levels=data$Gene.Set.Name)
+p=ggplot(data,aes(x=Gene.Set.Name,y=overlap,size=log10q,color=group))+theme_bw()+geom_point()+coord_flip()
+pp=p+ylim(-0.3,0.3)
+pp+theme(axis.text.x=element_blank(),axis.text.y=element_blank(),axis.title.x=element_blank(),axis.title.y=element_blank(),axis.ticks.x=element_blank(),axis.ticks.y=element_blank())
 
+###plot x1 & x2
+library("ggplot2")
+data=read.delim("D:/demo/demo.txt",head=T,sep="\t")
+ls(data)
+p=ggplot(data,aes(x=value_1,y=value_2,color=group))+geom_point(size=2)+theme_bw()
+pp=p+scale_x_continuous(trans='log10')+scale_y_continuous(trans='log10')
+pp+geom_abline(intercept=0,slope=1)
 
 
 
